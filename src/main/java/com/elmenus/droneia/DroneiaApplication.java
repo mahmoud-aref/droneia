c942afbfb42096d54d2f68a4d0113615f71bc1e0
Mahmoud Aref <mahmoud.3aaref@gmail.com>
2024-04-04 22:59:01 +0200

    init project droneia

---
+++ src/main/java/com/elmenus/droneia/DroneiaApplication.java
@@ -0,0 +1,13 @@
+package com.elmenus.droneia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elmenus/droneia/DroneiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmenus.droneia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
