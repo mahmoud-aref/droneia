34e1ae826ae1f68739c436b5f0d4425d8b43263d
Mahmoud Aref <mahmoud.3aaref@gmail.com>
2024-04-05 20:01:44 +0200

    add database to compose file and app file

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,10 +22,24 @@ repositories {
 }
 
 dependencies {
+
+    // framework dependencies
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+
+    // testing dependencies
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // tooling
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // database
+    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("org.postgresql:r2dbc-postgresql")
+
 }
 
 tasks.withType<Test> {
